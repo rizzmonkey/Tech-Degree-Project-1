60af28bd88fcf76d867a0a27eceff0e030a1ff07
Ross Fruin <ross@gridrival.com>
2019-05-16 23:04:03 -0700

    Upload Project File

---
+++ PlayerList.playground/Contents.swift
@@ -0,0 +1,27 @@
+// Player List Dictionary
+
+let player1: [String: Any] = ["name": "Joe Smith", "height": 42.0, "experience": true, "guardians": "Jim and Jan Smith"]
+let player2: [String: Any] = ["name": "Jill Tanner", "height": 36.0, "experience": true, "guardians": "Clara Tanner"]
+let player3: [String: Any] = ["name": "Bill Bon", "height": 43.0, "experience": true, "guardians": "Sara and Jenny Bon"]
+let player4: [String: Any] = ["name": "Eva Gordon", "height": 45.0, "experience": false, "guardians": "Wendy and Mike Gordon"]
+let player5: [String: Any] = ["name": "Matt Gill", "height": 40.0, "experience": false, "guardians": "Charles and Sylvia Gill"]
+let player6: [String: Any] = ["name": "Kimmy Stein", "height": 41.0, "experience": false, "guardians": "Bill and Hillary Stein"]
+let player7: [String: Any] = ["name": "Sammy Adams", "height": 45.0, "experience": false, "guardians": "Jeff Adams"]
+let player8: [String: Any] = ["name": "Karl Saygan", "height": 42.0, "experience": true, "guardians": "Heather Bledsoe"]
+let player9: [String: Any] = ["name": "Suzane Greenberg", "height": 44.0, "experience": true, "guardians": "Henrietta Dumas"]
+let player10: [String: Any] = ["name": "Sal Dali", "height": 41.0, "experience": false, "guardians": "Gala Dali"]
+let player11: [String: Any] = ["name": "Joe Kavalier", "height": 39.0, "experience": false, "guardians": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["name": "Ben Finklestein", "height": 44.0, "experience": false, "guardians": "Aaron and Jill Finklestein"]
+let player13: [String: Any] = ["name": "Diego Soto", "height": 41.0, "experience": true, "guardians": "Robin and Sarika Soto"]
+let player14: [String: Any] = ["name": "Chloe Alaska", "height": 47.0, "experience": false, "guardians": "David and Jamie Alaska"]
+let player15: [String: Any] = ["name": "Arnold Willis", "height": 43.0, "experience": false, "guardians": "Claire Willis"]
+let player16: [String: Any] = ["name": "Phillip Helm", "height": 44.0, "experience": true, "guardians": "Thomas Helm and Eva Jones"]
+let player17: [String: Any] = ["name": "Les Clay", "height": 42.0, "experience": true, "guardians": "Wynonna Brown"]
+let player18: [String: Any] = ["name": "Herschel Krustofski", "height": 45.0, "experience": true, "guardians": "Hyman and Rachel Krustofski"]
+
+// Empty Collection Constant
+
+let playerName = [String: Any] ()
+let playerHeight = [String: Any] ()
+let playerExperience = [String: Any] ()
+let playerGaurdian = [String: Any] ()
